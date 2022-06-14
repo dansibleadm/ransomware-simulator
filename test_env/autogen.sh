4c6bbba57dc9613b6a2a72ff15dedf6866c5af02
dansibleadm <bandaniel420@gmail.com>
2022-06-14 08:10:55 +0300

    Changes for test_env

---
+++ test_env/autogen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# test.txt
+mkdir /home/test
+base64 < /dev/urandom | head -c 1404 > /home/test/test.txt && cp /home/test/test.txt /etc/test.txt
+
+# new_test.txt
+base64 < /dev/urandom | head -c 4000 > /new_test.txt
+
+# flag.txt
+base64 < /dev/urandom | head -c 666 > /tmp/flag.txt && cp /tmp/flag.txt /usr/flag.txt
